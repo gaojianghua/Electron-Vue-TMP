6001e7784b0bf9f4d697377f7dbd44562b3ac080
高江华 <g598670138@163.com>
2023-06-29 17:05:22 +0800

    初始化项目搭建

---
+++ src/main/mainEntry.ts
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from "electron";
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+let mainWindow: BrowserWindow;
+
+app.whenReady().then(() => {
+    let config = {
+        webPreferences: {
+            nodeIntegration: true,
+            webSecurity: false,
+            allowRunningInsecureContent: true,
+            contextIsolation: false,
+            webviewTag: true,
+            spellcheck: false,
+            disableHtmlFullscreenWindowResize: true,
+        },
+    };
+    mainWindow = new BrowserWindow(config);
+    mainWindow.webContents.openDevTools({ mode: "undocked" });
+    mainWindow.loadURL(process.argv[2]);
+});
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// @ts-ignore
+import {devPlugin} from './plugins/devPlugin';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [devPlugin(), vue()],
+})
